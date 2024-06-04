23912b88b0ce23989882fdfcbcb6ff398a5b7682
kakon-selise <kakon.mehedi@selise.ch>
2024-06-04 12:10:41 +0600

    feat: Add async await code

---
+++ promise/src/async-await.ts
@@ -0,0 +1,85 @@
+namespace AsyncAwait {
+	const isValidCart = false;
+	const isValidOrderId = false;
+	const isValidOrderSummary = false;
+	const isValidWallet = false;
+
+	const cart = ['shirt', 'pant', 'shoes'];
+
+	function createOrder(cart: any): Promise<{ orderId: string }> {
+		const createOrderPromise: Promise<{ orderId: string }> = new Promise(
+			(resolve, reject) => {
+				if (isValidCart) {
+					const data = { orderId: 'demoOrderId' };
+					resolve(data);
+				} else {
+					const err = new Error('Cart is not valid');
+					reject(err);
+				}
+			}
+		);
+
+		return createOrderPromise;
+	}
+
+	function proceedToPayment(orderId: string): Promise<{ data: any }> {
+		const paymentPromise: Promise<{ data: any }> = new Promise(
+			(resolve, reject) => {
+				if (isValidOrderId) {
+					resolve({ data: 'This is dummy data' });
+				} else {
+					const err = new Error('Order id is not valid');
+					reject(err);
+				}
+			}
+		);
+
+		return paymentPromise;
+	}
+
+	function showOrderSummary(
+		orderData: any
+	): Promise<{ totalAmount: number }> {
+		const orderSummaryPromise: Promise<{ totalAmount: number }> =
+			new Promise((resolve, reject) => {
+				if (isValidOrderSummary) {
+					resolve({ totalAmount: 500 });
+				} else {
+					const err = new Error('Order summary is not valid');
+					reject(err);
+				}
+			});
+
+		return orderSummaryPromise;
+	}
+
+	function updateWallet(totalAmount: number): Promise<{ wallet: any }> {
+		const updateWalletPromise: Promise<{ wallet: any }> = new Promise(
+			(resolve, reject) => {
+				if (isValidWallet) {
+					resolve({ wallet: 'Valid wallet' });
+				} else {
+					const err = new Error('Wallet is not valid');
+					reject(err);
+				}
+			}
+		);
+
+		return updateWalletPromise;
+	}
+
+	async function manageOrder() {
+		try {
+			const orderData = await createOrder(cart);
+			const paymentData = await proceedToPayment(orderData.orderId);
+			const amountInfo = await showOrderSummary(paymentData.data);
+			const updatedWallet = await updateWallet(amountInfo.totalAmount);
+
+			return updatedWallet;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+    manageOrder();
+}
